74cb4a53d97494b0a215e0c3d56d51f7e4856da3
Johnny Dodson <59705816+jododson@users.noreply.github.com>
2020-12-03 12:47:31 -0500

    Add Task to course
    due date's aren't functioning *quite* right tho

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -2,6 +2,7 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">{{ data.title }}</h5>
+        <button v-on:click="addTask" class="btn btn-putline-primary my-2 mh-sm-0">Add Task</button>
         <ul>
             <li v-for='(reminder, index) in data.reminders' :key="index">{{ reminder.name }}, Due: {{ Date(reminder.due) }}</li>
         </ul>
@@ -10,10 +11,33 @@
 </template>
 
 <script>
+
+import Vue from 'vue'
+import Task from '@/components/Task.vue'
+
 export default {
   name: 'Course',
   props: {
-    data: Object
+    data: Object,
+    title: String
+  },
+  data: function(){
+    return{
+      reminders: []
+    }
+  },
+  methods: {
+    addTask: function(){
+      this.$dialog.prompt("Input task title",Text).then(result=>{
+        var ComponentClass = Vue.extend(Task)
+        var instance = new ComponentClass()
+        instance.name = result.data
+        this.$dialog.prompt("Input due date",Date).then(result=>{
+          instance.dueDate = result.data
+          this.data.reminders.push(instance)
+        })
+      })
+    }
   }
 }
 </script>
---
+++ src/components/Task.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+      <div class="card-body">
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    data: Object,
+    title: String
+  },
+  data: function(){
+    return{
+      name: String,
+      due: Date
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
